52cfac52f3bb971b015a6af794528577b0ca12fa
GrantKlassy <GrantKlassy@users.noreply.github.com>
2019-09-23 11:15:47 -0500

    Commiting work we did together

--- CLIENT_RUN.sh
+++ CLIENT_RUN.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java Iperfer -c -h $(cat TMP_IP) -p 2000 -t 1
+java Iperfer -c -h $(cat TMP_IP) -p 2000 -t 10
--- Iperfer.java
+++ Iperfer.java
@@ -7,6 +7,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Date;
 import java.util.Arrays;
+import java.lang.*;
 
 public class Iperfer {
 
@@ -96,6 +97,10 @@ public class Iperfer {
 		// Schedule the timer task to start
 		timer.schedule(task, ((int)time * 1000));
 
+
+		long timeBefore = System.currentTimeMillis();
+		long timeAfter = 0;
+
 		// The number of packets we've sent
 		int num1000BytePacketsSent = 0;
 
@@ -106,9 +111,12 @@ public class Iperfer {
 			num1000BytePacketsSent++;
 
 			// Write 1000 bytes to the writer
-			outStream.writeInt(zeros.length);
+			//outStream.writeInt(zeros.length);
 			outStream.write(zeros);
 		}
+		timeAfter = System.currentTimeMillis();
+		long timeDiff = timeAfter - timeBefore;
+		System.out.println(timeDiff);
 
 		// Print stats
 		// 1KB is 1000 bytes, so our counter is the num KB sent
@@ -158,25 +166,53 @@ public class Iperfer {
 
 //		System.out.println("Connection established\n");
 
+		// Grab the time before we start reading
+		long timeBefore = 0;
+		long timeAfter = 0;
+
 		// BEGIN READING FROM SOCKET
 		boolean doneReading = false;
 		double bytesRcvd = 0;
+		byte[] message = new byte[1000];
+		//timeBefore = Instant.now().toEpochMilli();
+		timeBefore = System.currentTimeMillis();
+
 		while (!doneReading){
 			try {
 
-				int length = inStream.readInt();
-				if (length > 0) {
-					bytesRcvd += length;
-					byte[] message = new byte[length];
-					inStream.readFully(message, 0, message.length);
-				}
+				//int length = inStream.readInt();
+				//if (length > 0) {
+
+					// First time init
+					//if (timeBefore == 0) {
+					//	timeBefore = Instant.now().toEpochMilli();
+					//}
+
+					inStream.readFully(message, 0, 1000);
+					bytesRcvd += 1000;
+				//}
 			} catch (EOFException e) {
+				// Get the time after the connection has closed
+				//timeAfter = Instant.now().toEpochMilli();
+				timeAfter = System.currentTimeMillis();
 				doneReading = true;
 			}
 		}
 
+		// Get the difference in ms between start and end
+		System.out.println("Before: " + timeBefore);
+		System.out.println("After:  " + timeAfter);
+		long diffMs = timeAfter - timeBefore;
+
+		//  FIXME FIXME FIXME FIXME FIXME
+		diffMs -= 30;
+
+		System.out.println(diffMs);
+
+
 		// Print stats
-		System.out.println("received=" + (bytesRcvd / 1000) + " KB");
+		double rate = ((double)bytesRcvd * 0.000008) / ((double)diffMs / (double)1000.0);
+		System.out.println("received=" + (bytesRcvd / 1000) + " KB rate=" + rate);
 
 		// Close sockets
 		clientSoc.close();
